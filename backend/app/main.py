11eae1145a7466289f0190ec8d4c24c4e80645a3
evanbones <74276616+evanbones@users.noreply.github.com>
2025-10-19 20:45:11 -0700

    Add pytest + requirements + placeholders
    Fix requirements to build on Windows, add placeholder pytest code

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+import uvicorn
 from routers.items import router as items_router, movieRouter as movies_router
 
 app = FastAPI()
@@ -9,3 +10,6 @@ def health():
 
 app.include_router(items_router)
 app.include_router(movies_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)

361847d5026e195f549ead50eb68e1e8b7fa5317
Patrik Balazsy <patrikpbalazsy@gmail.com>
2025-11-26 01:39:17 -0800

    Improved previous fix to first check for duplicates and updated tests

--- backend/tests/unit/core/test_genome_processor.py
+++ backend/tests/unit/core/test_genome_processor.py
@@ -5,12 +5,10 @@ from app.ml.feature_engineering.genome_processor import create_genome_features
 
 
 def test_genome_duplicates(mocker):
-    mock_logger = mocker.patch("app.ml.feature_engineering.genome_processor.logger")
-
     movies_df = pd.DataFrame({"movie_id": [100, 101]})
     genome_df = pd.DataFrame(
         {
-            "movie_id": [100, 101, 101],  # Duplicate movie_id
+            "movie_id": [100, 101, 101],  # Duplicate movie_id with same tag_id
             "tag_id": [1, 1, 1],
             "relevance": [0.9, 0.4, 0.8],
         }
@@ -19,5 +17,7 @@ def test_genome_duplicates(mocker):
     result = create_genome_features(movies_df, genome_df)
 
     assert result.shape == (2, 1)
+
     assert result[0][0] == pytest.approx(0.9)
-    assert result[1][0] == pytest.approx(0.6)
+
+    assert result[1][0] == pytest.approx(0.4)

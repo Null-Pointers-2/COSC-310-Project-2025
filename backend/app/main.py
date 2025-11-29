f7223f711eef226d5f75eeb9c15923c4d9e9a30f
Patrick Rinn <144386801+PatrickRinn@users.noreply.github.com>
2025-11-28 23:59:34 -0800

    Add middleware

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,6 +4,7 @@ import logging
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.core.resources import SingletonResources
 from app.routers import (
@@ -55,6 +56,18 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/health", tags=["Health"])
 async def health_check():
